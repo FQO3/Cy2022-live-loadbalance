f3aaf3888304b71392e05ac4f4bf4fa451fbf7e0
FQO3 <111474347+FQO3@users.noreply.github.com>
2024-12-16 22:39:59 +0800

    2024.12.16 元旦晚会限定改版

--- www.js
+++ www.js
@@ -38,10 +38,10 @@ app.get("/", function (req, res) {
 app.use(express.static("PC"));
 app.post("/api/create/:id", async (req, res) => {
     var code = req.params;
-    const command = `ffmpeg -i rtmp://cyxsh.top:1935/live/${code.id} -c copy -f flv rtmp://127.0.0.1:1935/live/${code.id}`;
+    const command = `ffmpeg -i rtmp://fqo3.site:1935/live/${code.id} -c copy -f flv rtmp://127.0.0.1:1935/live/${code.id}`;
     try {
         const { stdout, stderr } = exec(command);
-        console.log(`已【成功】创建${code}的视频流`);
+        console.log(`已【成功】创建${code.id}的视频流`);
         res.sendStatus(200);
     } catch (error) {
         console.error(`Failed to execute command: ${error}`);
